2669d2696f705e4489b9de87fce435727094b3b5
Abdus Salam <freelancersalam5160@gmail.com>
2022-09-14 11:46:59 +0600

    string method used

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let fuss = "why you're fuss so much about this kind of stuff"; //variable created called fuss
 
 console.log(fuss.length); //length propertis returns the length of a string
+
+//extracting string method in javaScript
+
+function strFunction() {
+  let x = document.getElementById("demo").innerHTML;
+  alert("you're gonna see 'Lorem ipsum'");
+  document.getElementById("demo").innerHTML = x.substr(0, 25);
+}
